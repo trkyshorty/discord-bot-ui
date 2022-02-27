fba64fb46aaa3f3721ecac215bd2c653c517f3e5
Türkay Tanrıkulu <trky.shorty@gmail.com>
2022-02-28 02:26:33 +0300

    Update actions.js

--- store/guild/actions.js
+++ store/guild/actions.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default {
   getGuild({ commit, state, rootState }, payload) {
     return new Promise((resolve, reject) => {
-      axios.get(`${process.env.API_URL}/guild/${payload.guild_id}`, {
+      axios.get(`${process.env.API_URL}/guilds/${payload.guild_id}`, {
         headers: {
           Authorization: "Bearer " + rootState.user.token
         }
